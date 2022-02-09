d02395c38c13be36a98612b00a826be79b922aea
Stefania Mauti <s.mauti@interface3.be>
2022-02-09 17:25:26 +0100

    all

---
+++ ASM-wad21/JS/scrip.js
@@ -0,0 +1,36 @@
+function setImg() {
+    let section = document.getElementById("spazio1");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : Belgique</b> <br> Capitale : Bruxelles<br> Superficie : 30 528 km²<br> Population : 11 498 519<br> Régime politique : Monarchie constitutionnelle parlementaire fédérale<br> Devise nationale : L'union fait la force - Eendracht maakt macht - Einigkeit macht stark<br> Hymne national : La Brabançonne<br> Langues officielles : Allemand, Français, Néerlandais<br> Monnaie : Euro (EUR), depuis 1999<br> Pays limitrophes : Allemagne, France, Luxembourg, Pays-Bas<br> Ouverture sur la mer : mer du Nord<br>";
+}
+function setImg2() {
+    let section = document.getElementById("spazio2");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : Malaisie</b><br> Capitale : Kuala Lumpur<br> Superficie : 330 803 km²<br> Population : 32 042 458<br> Régime politique : Monarchie constitutionnelle<br> Devise nationale : Bersekutu Bertambah Mutu (L'unité est la force)<br> Hymne national : Negaraku (Mon pays)<br> Langue officielle : Malaisien<br> Monnaie : Ringgit (MYR), depuis 1967<br> Pays limitrophes : Brunei, Indonésie, Singapour, Thaïlande<br>";
+}
+function setImg3() {
+    let section = document.getElementById("spazio3");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : Etats-Unis</b><br> Capitale : Washington<br> Superficie : 9 371 180 km²<br> Population : 326 766 748<br> Régime politique : République constitutionnelle présidentielle fédérale<br> Devise nationale : In God We Trust (En Dieu nous croyons)<br> Hymne national : The Star-Spangled Banner<br> Langue officielle : Anglais<br> Monnaie : Dollar américain (USD)<br> Pays limitrophes : Canada, Mexique<br> Ouverture sur la mer : océan Atlantique, océan Pacifique<br>";
+}
+function setImg4() {
+    let section = document.getElementById("spazio4");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : République Démocratique du Congo</b><br> Capitale : Kinshasa<br> Superficie : 2 344 860 km²<br> Population : 84 004 989<br> Régime politique : République<br> Devise nationale : Paix, Justice, Travail<br>Hymne national : Debout Congolais<br> Langue officielle : Français<br> Monnaie : Franc congolais (CDF)<br> Pays limitrophes : Angola, Angola, Burundi, Congo, Ouganda, Rwanda, République Centrafricaine, Soudan du Sud, Tanzanie, Zambie<br> Ouverture sur la mer : océan Atlantique<br>";
+}
+function setImg5() {
+    let section = document.getElementById("spazio5");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : Russie</b><br> Capitale : Moscou<br> Superficie : 17 098 200 km²<br> Population : 143 964 709<br> Régime politique : République fédérale<br> Hymne national : Gosudarstvenny gimn Rossiyskoy Federatsii (Hymne national de la Fédération de Russie)<br> Langue officielle : Russe<br> Monnaie : Rouble russe (RUB), depuis 1998<br> Pays limitrophes : Azerbaïdjan, Biélorussie, Chine, Corée du Nord, Estonie, Finlande, Géorgie, Kazakhstan, Lettonie, Lituanie, Mongolie, Norvège, Pologne, Ukraine<br>Ouverture sur la mer : mer Baltique, mer Caspienne, mer Noire, mer d'Azov, mer de Barents, océan Arctique, océan Pacifique<br>";
+}
+function setImg6() {
+    let section = document.getElementById("spazio6");
+    let p_testo=document.createElement('p');
+    section.appendChild(p_testo);
+    p_testo.innerHTML="<b>Nom du pays : Chine </b><br> Capitale : Pékin<br> Superficie : 9 598 000 km²<br> Population : 1 415 045 928<br> Régime politique : République<br> Hymne national : La Marche des Volontaires<br> Langue officielle : Chinois mandarin<br> Monnaie : Yuan (CNY)<br> Pays limitrophes : Afghanistan, Bhoutan, Birmanie, Corée du Nord, Inde, Kazakhstan, Kirghizistan, Laos, Mongolie, Népal, Pakistan, Russie, Tadjikistan, Viêt Nam<br> Ouverture sur la mer : mer Jaune, mer de Chine méridionale, mer de Chine orientale<br>";
+}
